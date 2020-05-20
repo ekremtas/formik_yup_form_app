c41968ddf7d3d7e3a6d943acacd124ef440c173d
Ekrem TAŞ <ekremtas589@gmail.com>
2020-05-20 04:52:54 +0300

    Updates

---
+++ precache-manifest.b6814e569054610ed5d65be394a3ed7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f84be88154b266d0143b72acbede2f5",
+    "url": "/formik_yup_form_app/index.html"
+  },
+  {
+    "revision": "f9412ea4dc190cb40ce4",
+    "url": "/formik_yup_form_app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "64236f743808fdf62486",
+    "url": "/formik_yup_form_app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f9412ea4dc190cb40ce4",
+    "url": "/formik_yup_form_app/static/js/2.27f53100.chunk.js"
+  },
+  {
+    "revision": "e2efa734baa9e5c3b8ef165e6121d69e",
+    "url": "/formik_yup_form_app/static/js/2.27f53100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64236f743808fdf62486",
+    "url": "/formik_yup_form_app/static/js/main.11b1dbe5.chunk.js"
+  },
+  {
+    "revision": "294607b4dcc2ab1f2aef",
+    "url": "/formik_yup_form_app/static/js/runtime-main.a9ac7b35.js"
+  }
+]);
